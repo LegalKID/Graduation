03d84afdd0cf2bdb3c5a54312e3b5c3e19171f02
LegalKID <150474419+LegalKID@users.noreply.github.com>
2024-04-13 21:31:52 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,19 @@
+# server.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hakkinda')
+def hakkinda():
+    return render_template('about.html')
+
+@app.route('/degisim')
+def degisim():
+    return render_template('iklim_degisimi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
